771bc7cff5846ce03d32bee2ef4c9c664529822f
Delta <deltarune5609@gmail.com>
2026-09-20 10:15:09 -0400

    Change Next Configs

--- event-platform/middleware.ts
+++ event-platform/middleware.ts
@@ -1,4 +1,4 @@
-import { NextResponse, type NextRequest } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 import { createServerClient } from '@supabase/ssr'
 
 export async function middleware(request: NextRequest) {
